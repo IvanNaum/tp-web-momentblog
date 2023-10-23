32bd805be155bb7df09b625f390ab4e3a9888048
IvanNaum <nuk04@mail.ru>
2023-10-24 01:50:35 +0300

    added validation in form

--- frontend/src/components/Pages/Login/LoginPage.js
+++ frontend/src/components/Pages/Login/LoginPage.js
@@ -1,21 +1,53 @@
-import { Container, Form, Button } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
 
 const LoginPage = () => {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
   return (
     <Container className="d-flex flex-column align-items-center mt-5">
       <h2>Авторизация</h2>
-      <Form className="w-75">
+      <Form
+        noValidate
+        validated={validated}
+        onSubmit={handleSubmit}
+        className="w-75"
+      >
         <Form.Group className="mb-2" controlId="email_nickname">
           <Form.Label>Email или nickname</Form.Label>
           <Form.Control
             type="text"
             placeholder="Введите email или nickname..."
+            aria-describedby="email_nickname"
+            // pattern={validEmail}
+            required
           />
+          <Form.Control.Feedback type="invalid">
+            Введите корректный email или nickname
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-2" controlId="password">
           <Form.Label>Пароль</Form.Label>
-          <Form.Control type="password" placeholder="Введите пароль..." />
+          <Form.Control
+            type="password"
+            placeholder="Введите пароль..."
+            aria-describedby="password"
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Введите корректный пароль
+          </Form.Control.Feedback>
         </Form.Group>
         <div className="w-100 my-3">
           <Button
--- frontend/src/components/Pages/Login/RegistrationPage.js
+++ frontend/src/components/Pages/Login/RegistrationPage.js
@@ -1,20 +1,54 @@
 import { Container, Form, Button, InputGroup } from "react-bootstrap";
+import { useState } from "react";
 
 const RegistrationPage = () => {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
   return (
     <Container className="d-flex flex-column align-items-center mt-5">
       <h2>Регистрация</h2>
-      <Form className="w-75">
+      <Form
+        className="w-75"
+        noValidate
+        validated={validated}
+        onSubmit={handleSubmit}
+      >
         <Form.Group className="mb-2" controlId="email">
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Введите email..." />
+          <Form.Control
+            type="email"
+            placeholder="Введите email..."
+            aria-describedby="email"
+            // TODO add email validator
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Введите корректный email
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-2" controlId="nickname">
           <Form.Label>Nickname</Form.Label>
           <InputGroup className="mb-3">
             <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
-            <Form.Control placeholder="Введите nickname..." />
+            <Form.Control
+              placeholder="Введите nickname..."
+              aria-describedby="nickname"
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Введите корректный nickname
+            </Form.Control.Feedback>
           </InputGroup>
         </Form.Group>
 
@@ -24,12 +58,25 @@ const RegistrationPage = () => {
             type="file"
             accept="image/*"
             placeholder="Добавьте фотографию..."
+            aria-describedby="imagefile"
+            required
           />
+          <Form.Control.Feedback type="invalid">
+            Добавьте корректную фотографию
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-2" controlId="password1">
           <Form.Label>Пароль</Form.Label>
-          <Form.Control type="password" placeholder="Введите пароль..." />
+          <Form.Control
+            type="password"
+            placeholder="Введите пароль..."
+            aria-describedby="password1"
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Введите корректный пароль
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-2" controlId="password2">
@@ -37,8 +84,14 @@ const RegistrationPage = () => {
           <Form.Control
             type="password"
             placeholder="Введите пароль ещё раз..."
+            aria-describedby="password2"
+            required
           />
+          <Form.Control.Feedback type="invalid">
+            Введите корректный пароль
+          </Form.Control.Feedback>
         </Form.Group>
+
         <div className="w-100 my-3">
           <Button className="d-block mx-auto" variant="primary" type="submit">
             Зарегистрироваться
