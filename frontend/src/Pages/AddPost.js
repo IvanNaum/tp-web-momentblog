d054a7fa9071d4189b19b80b5a0f1a83497d171a
IvanNaum <nuk04@mail.ru>
2023-10-28 21:32:32 +0300

    added addpost page validator

--- frontend/src/Pages/AddPost.js
+++ frontend/src/Pages/AddPost.js
@@ -1,13 +1,38 @@
 import { Container, Form, Button } from "react-bootstrap";
+import { useState } from "react";
 
 const AddPostPage = () => {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
   return (
     <Container className="d-flex flex-column align-items-center mt-5">
       <h2>Добавить момент</h2>
-      <Form className="w-75">
+      <Form
+        noValidate
+        validated={validated}
+        onSubmit={handleSubmit}
+        className="w-75"
+      >
         <Form.Group className="mb-2" controlId="description">
           <Form.Label>Описание</Form.Label>
-          <Form.Control type="text" placeholder="Введите описание..." />
+          <Form.Control
+            type="text"
+            placeholder="Введите описание..."
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Введите корректное описание
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-2" controlId="imagefile">
@@ -16,7 +41,11 @@ const AddPostPage = () => {
             type="file"
             accept="image/*"
             placeholder="Добавьте фотографию..."
+            required
           />
+          <Form.Control.Feedback type="invalid">
+            Введите корректную фотографию
+          </Form.Control.Feedback>
         </Form.Group>
 
         <div className="w-100 my-3">
