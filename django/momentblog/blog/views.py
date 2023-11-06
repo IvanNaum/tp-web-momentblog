cd4de5ba4190b571aec079cadee10c7b00cb607f
IvanNaum <nuk04@mail.ru>
2023-11-06 16:58:53 +0300

    tested drf

--- django/momentblog/blog/views.py
+++ django/momentblog/blog/views.py
@@ -1,5 +1,10 @@
-from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
+@api_view(['GET'])
 def test(request):
-    return HttpResponse('Hello, welcome to the index page.')
+    data = {
+        "test": "test text"
+    }
+    return Response(data)
