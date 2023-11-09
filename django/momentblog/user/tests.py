cf3e86d9eb2fbd664300ea34b59acbdb51fd02ad
IvanNaum <nuk04@mail.ru>
2023-11-09 11:54:55 +0300

    edited user/tests 2

--- django/momentblog/user/tests.py
+++ django/momentblog/user/tests.py
@@ -24,21 +24,13 @@ class UsersManagersTests(TestCase):
         with self.assertRaises(TypeError):
             User.objects.create_user(email="test@test.tu", password="foo")
 
-def test_create_superuser(self):
-    User = get_user_model()
-    admin_user = User.objects.create_superuser(username="test",
-                                               email="super@user.com",
-                                               password="foo")
-    self.assertEqual(admin_user.email, "super@user.com")
-    self.assertTrue(admin_user.is_active)
-    self.assertTrue(admin_user.is_staff)
-    self.assertTrue(admin_user.is_superuser)
-    try:
-        # username is None for the AbstractUser option
-        # username does not exist for the AbstractBaseUser option
-        self.assertIsNone(admin_user.username)
-    except AttributeError:
-        pass
-    with self.assertRaises(ValueError):
-        User.objects.create_superuser(
-            email="super@user.com", password="foo", is_superuser=False)
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(username="test",
+                                                   email="super@user.com",
+                                                   password="foo")
+        self.assertEqual(admin_user.email, "super@user.com")
+        self.assertEqual(admin_user.username, "test")
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
